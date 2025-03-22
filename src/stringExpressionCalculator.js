a7959bbe86793b19415b62c63bd9c8a1bfda7164
ksahu <kanishksahu@gmail.com>
2025-03-22 19:48:51 +0530

    Update add function to pass test for change delimiter in string

--- src/stringExpressionCalculator.js
+++ src/stringExpressionCalculator.js
@@ -2,7 +2,10 @@ const add = (inputString) => {
   
     if(inputString=="") return 0;
 
-    const numbers = inputString.split(/\n|,/);
+    const {input, delimiter} = getDelimiter(inputString);
+
+    // Split the remaining string by the delimiter and map it to numbers
+    const numbers = input.split(new RegExp(`\\${delimiter}|\\n`));
 
     let sum = 0;
 
@@ -11,6 +14,23 @@ const add = (inputString) => {
     }
 
     return sum;
+}
+
+const getDelimiter = (input) => {
+    const delimiterMatch = input.match(/^\/\/(.*?)\n/);
+    let delimiter = ',';
+    
+    // If a delimiter is found, use it; otherwise, default to ","
+    if (delimiterMatch) {
+      delimiter = delimiterMatch[1];
+      // Remove the first line (which contains the delimiter) and the newline
+      input = input.slice(delimiterMatch[0].length);
+    }
+  
+    return {
+        delimiter,
+        input
+    }
 }
 
 module.exports = add;
